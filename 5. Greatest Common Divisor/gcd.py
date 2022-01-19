2b83559599ce4ed9755fac6ce459b88bd119c194
Ankit <lamsalankit@gmail.com>
2022-01-19 10:32:20 +0545

    Vigerene Cipher Completed.

---
+++ 5. Greatest Common Divisor/gcd.py
@@ -0,0 +1,11 @@
+def gcd(a,b):
+    if a == 0:
+        return b
+    else:
+        return gcd(b%a,a)
+
+a  =  int(input("Enter the value of A:"))
+b =  int(input("Enter the value of B:"))
+r =  gcd(a,b)
+print("Greatest Common Divisior of a and b is:"+str(r))
+
